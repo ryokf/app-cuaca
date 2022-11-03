bc41fc28f065c2d96530a015bf3edb3ccaab7a7b
ryokf <ryokhrisnaf@gmail.com>
2022-11-03 15:14:10 +0700

    coba ganti desain

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,17 @@
+function cuaca(cuaca){
+    if(cuaca === 'awan pecah' || cuaca === 'awan tersebar' || cuaca === 'langit cerah'){
+        return 'cerah';
+    } else if(cuaca === 'sedikit berawan'){
+        return 'cerah berawan';
+    } else if(cuaca === 'awan mendung'){
+        return 'berawan';
+    } else if(cuaca === 'hujan sedang' || cuaca === 'hujan ringan' || cuaca === 'hujan rintik-rintik'){
+        return 'hujan ringan';
+    }else if(cuaca === 'salju ringan'){
+        return cuaca;
+    }
+}
+
 function today(date){
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -7,22 +21,39 @@ function today(date){
     return `${days[today.getDay()]}, ${today.getDate()} ${months[today.getMonth()]} ${today.getFullYear()}`
 }
 
-$.ajax({
-    type: "GET",
-    url: "http://api.openweathermap.org/data/2.5/weather",
-    data: {
-        'q': 'semarang',
-        'APPID': '8dd8a74df072f100f5a98c071d528825',
-        'lang': 'id',
-        'units': 'metric',
-    },
-    dataType: "json",
-    success: function (response) {
-        console.log(response);
-        $('#nama-kota').append(response.name);
-        $('#tanggal').append(today(response.dt * 1000));
-    },
-    error: function () {
-        $('h1').append('none');
-    }
-});
+$('#tombol-cari').on('click', function(){
+    $('#nama-kota').html('');
+    $('#tanggal').html('');
+    $('#suhu-celcius').html('');
+    $('#suhu-fahrenheit').html('');
+    $('#cuaca').html('');
+
+    $.ajax({
+        type: "GET",
+        url: "http://api.openweathermap.org/data/2.5/weather",
+        data: {
+            'q': `${$('#input-cari').val()}`,
+            // 'q': 'semarang',
+            'APPID': '8dd8a74df072f100f5a98c071d528825',
+            'lang': 'id',
+            'units': 'metric',
+        },
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            $('#nama-kota').append(response.name);
+            $('#tanggal').append(today(response.dt * 1000));
+            $('#suhu-celcius').append(Math.round(response.main.temp) + '<span><sup>o</sup>C</span>/');
+            $('#suhu-fahrenheit').append(Math.round(response.main.temp) + '<span><sup>o</sup>F</span>');
+            $('#cuaca').append(cuaca(response.weather[0].description));
+            $('img').attr('src', `dist/img/${cuaca(response.weather[0].description)}.png`);
+            // $('#cuaca').append(response.weather[0].description);
+
+            $('#input-cari').val('');
+        },
+        error: function () {
+            $('h1').append('none');
+        }
+    });
+})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily : {
-        'inter' : ['Inter', 'sans-serif'],
+        'inter' : ['Lexend Deca', 'sans-serif'],
       },
     },
   },
